55d611a015d9deed797f2b565a95d2828eadf601
Kipras <kiprasjanuska0@gmail.com>
2023-01-13 18:09:15 -0500

    Update restart script

--- restart.sh
+++ restart.sh
@@ -1,4 +1,26 @@
+install_path=$(pwd)
+
+echo "Removing zebra service if it exists:"
 sudo systemctl stop zebra
+sudo rm -rf /etc/systemd/system/zebra.*
+sudo systemctl daemon-reload
+echo "...done"
+
+echo "Creating zebra service:"
+sudo cp ./config/zebra.service /etc/systemd/system/
+sudo sed -i -e "/\[Service\]/a ExecStart=python ${install_path}/python/client/app.py &" /etc/systemd/system/zebra.service
 sudo systemctl daemon-reload
 sudo systemctl start zebra
 sudo systemctl enable zebra
+echo "...done"
+
+echo -n "In order to finish setup a reboot is necessary..."
+echo -n "REBOOT NOW? [y/N] "
+read
+if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then
+        echo "Exiting without reboot."
+        exit 0
+fi
+echo "Reboot started..."
+reboot
+sleep infinity
